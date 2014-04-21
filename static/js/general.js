295122173f873c87d1f8251977a5934fb10e379b
yuya yano <yuya.yano.6260@gmail.com>
2014-04-21 18:30:47 +0900

    Added a github commit sample.

--- static/js/general.js
+++ static/js/general.js
@@ -14,3 +14,38 @@ var editor = new Vue({
 		marked: marked
 	}
 })
+
+var apiUrl = 'https://api.github.com/repos/yymm/Yoichi/commits?per_page=3&sha='
+var commits = new Vue({
+	el: "#commits",
+	data: {
+		branch: "master"
+	},
+	created: function(){
+		this.$watch('branch', function() {
+			this.fetchData();
+		})
+	},
+	filters: {
+		truncate: function(value){
+			var newline = value.indexOf('\n');
+			return newline > -1 ? value.slice(0, newline) : value;
+		},
+		formatDate: function(value){
+			return value.replace(/T|Z/g, ' ');
+		}
+	},
+	methods: {
+		fetchData: function(){
+			// self = thisは肝。
+			// onload内のcommitsをVueオブジェクト内の参照にするために必須。
+			// thisを使うとonload実行時のthis(=多分windowオブジェクト)になってしまう。
+			var xhr = new XMLHttpRequest(), self = this;
+			xhr.open('GET', apiUrl + self.branch);
+			xhr.onload = function(){
+				self.commits = JSON.parse(xhr.responseText);
+			}
+			xhr.send();
+		}
+	}
+})

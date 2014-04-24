8466d488694b3fedcf0823b7c9679d96f81868a5
yuya yano <yuya.yano.6260@gmail.com>
2014-04-24 11:28:21 +0900

    Stop experiments...

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/experiment')
+def experiment():
+    return render_template('experiment.html')
+
 if __name__ == '__main__':
     app.run(debug=True)

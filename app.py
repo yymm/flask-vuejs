d866f9af5de716b9836f7e18508b7cf175b740a2
yuya yano <yuya.yano.6260@gmail.com>
2017-03-16 12:44:53 +0900

    Starting migration to Vue2.

--- app.py
+++ app.py
@@ -1,14 +1,66 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+
+class CustomFlask(Flask):
+    jinja_options = Flask.jinja_options.copy()
+    jinja_options.update(dict(
+      block_start_string='{%',
+      block_end_string='%}',
+      variable_start_string='((',
+      variable_end_string='))',
+      comment_start_string='{#',
+      comment_end_string='#}',
+    ))
+
+
+app = CustomFlask(__name__)
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/experiment')
-def experiment():
-    return render_template('experiment.html')
+
+@app.route('/example')
+def example():
+    message = "Hello Flask!"
+    return render_template('example.html', message=message)
+
+
+@app.route('/sfc')
+def sfc():
+    return render_template('sfc.html')
+
+
+@app.route('/more')
+def more():
+    return render_template('more.html')
+
+
+@app.route('/axios')
+def axios():
+    return render_template('axios.html')
+
+
+@app.route('/router')
+def router():
+    return render_template('router.html')
+
+
+@app.route('/vuex')
+def vuex():
+    return render_template('vuex.html')
+
+
+@app.route('/webpack')
+def webpack():
+    return render_template('webpack.html')
+
+
+@app.route('/v0.10.3')
+def v0_10_3():
+    return render_template('vue.js_v0.10.3.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
